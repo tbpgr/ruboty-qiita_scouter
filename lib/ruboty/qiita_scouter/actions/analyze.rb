d1d67722953d70a543abab2f22895814bc431590
tbpgr <tbpgr@tbpgr.jp>
2014-10-28 22:52:43 +0900

    remove Constants

--- lib/ruboty/qiita_scouter/actions/analyze.rb
+++ lib/ruboty/qiita_scouter/actions/analyze.rb
@@ -1,10 +1,8 @@
+# rubocop:disable LineLength
 module Ruboty
   module QiitaScouter
     # Actions
     module Actions
-      DISPLAY_FORMAT = "ユーザー名: %s 戦闘力: %s 攻撃力: %s 知力: %s すばやさ: %s"
-      NOT_EXIST_MESSAGE = 'Failed by %s'
-
       # Analyze Qiita power
       class Analyze < Ruboty::Actions::Base
         def call
@@ -15,9 +13,9 @@ module Ruboty
 
         def analyze
           power_levels = ::QiitaScouter::Core.new.analyze(id)
-          format(DISPLAY_FORMAT, id, *power_levels)
+          format("ユーザー名: %s 戦闘力: %s 攻撃力: %s 知力: %s すばやさ: %s", id, *power_levels)
         rescue => e
-          format(NOT_EXIST_MESSAGE, e.class.name)
+          format("Failed by %s", e.class.name)
         end
 
         def id
@@ -27,3 +25,4 @@ module Ruboty
     end
   end
 end
+# rubocop:enable LineLength
